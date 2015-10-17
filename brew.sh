a41db57c4fdaed631c6f46926efc5581312c51d6
Ross <ross@Rosss-MacBook-Pro.local>
2015-10-18 00:42:52 +0100

    editted mac settings to my preferences

--- brew.sh
+++ brew.sh
@@ -38,15 +38,18 @@ brew install wget --with-iri
 # Install RingoJS and Narwhal.
 # Note that the order in which these are installed is important;
 # see http://git.io/brew-narwhal-ringo.
-brew install ringojs
-brew install narwhal
+
+#don't need js stuff
+
+#brew install ringojs
+#brew install narwhal
 
 # Install more recent versions of some OS X tools.
 brew install vim --override-system-vi
 brew install homebrew/dupes/grep
 brew install homebrew/dupes/openssh
 brew install homebrew/dupes/screen
-brew install homebrew/php/php55 --with-gmp
+#brew install homebrew/php/php55 --with-gmp
 
 # Install font tools.
 brew tap bramstein/webfonttools
@@ -55,45 +58,56 @@ brew install sfnt2woff-zopfli
 brew install woff2
 
 # Install some CTF tools; see https://github.com/ctfs/write-ups.
+
+#cracks wifi
 brew install aircrack-ng
+
+# java
 brew install bfg
+
+
 brew install binutils
 brew install binwalk
+
+
 brew install cifer
-brew install dex2jar
-brew install dns2tcp
+#brew install dex2jar
+#brew install dns2tcp
 brew install fcrackzip
 brew install foremost
-brew install hashpump
-brew install hydra
-brew install john
-brew install knock
-brew install netpbm
+#brew install hashpump
+#brew install hydra
+#brew install john
+#brew install knock
+#brew install netpbm
 brew install nmap
-brew install pngcheck
-brew install socat
-brew install sqlmap
-brew install tcpflow
-brew install tcpreplay
-brew install tcptrace
-brew install ucspi-tcp # `tcpserver` etc.
-brew install xpdf
-brew install xz
+#brew install pngcheck
+#brew install socat
+#brew install sqlmap
+#brew install tcpflow
+#brew install tcpreplay
+#brew install tcptrace
+#brew install ucspi-tcp # `tcpserver` etc.
+#brew install xpdf
+#brew install xz
 
 # Install other useful binaries.
 brew install ack
+
+#toggles dark mode on the os 
 brew install dark-mode
+
+
 #brew install exiv2
 brew install git
 brew install git-lfs
-brew install imagemagick --with-webp
+#brew install imagemagick --with-webp
 brew install lua
 brew install lynx
 brew install p7zip
 brew install pigz
 brew install pv
 brew install rename
-brew install rhino
 brew install speedtest_cli
 brew install ssh-copy-id
 brew install tree
